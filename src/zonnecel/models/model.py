691b0c1661ca916d180041e4143caf8fce15add6
rdvanturnhout <117833887+rdvanturnhout@users.noreply.github.com>
2022-12-06 12:44:14 +0100

    view and model

--- src/zonnecel/models/model.py
+++ src/zonnecel/models/model.py
@@ -2,10 +2,13 @@ from controllers.arduino_device import list_devices, ArduinoVISADevice
 
 print(list_devices())
 
-class DiodeExperiment:
+
+class ZonnecelExperiment:
 # Creating lists
     def __init__(self, port):
         self.device = ArduinoVISADevice(port)
+        self.voltages = []
+        self.currents = []
 
     def scan(self, start, stop):
         """Run experiment with ADC inputs in range(start, stop)
@@ -14,11 +17,19 @@ class DiodeExperiment:
             start (int): ADC value where the experiment starts (0 - 1023)
             stop (int): ADC value where the experiment stops (0 - 1023)
 
-
+        Return:
+            lists of voltages and currents
         """        
-        R = 220
+        R2 = 4.7
                 
-        for i in range (start,stop):
-            self.device.set_output_value(i)
-            U1 = 
-            U2 = 
+        for U0 in range (start,stop):
+            self.device.set_output_value(U0)
+            U1 = self.device.get_input_value(channel = 1) # spanningsmeter
+            U2 = self.device.get_input_value(channel = 2) # stroommeter
+            # U_zonnecel = 3 x U1
+            self.voltages.append(3*U1)
+            # I2 = U2 / R2
+            self.currents.append(float(U2)/R2)
+
+        # print(self.measurements.currents)
+        return self.voltages, self.currents
--- src/zonnecel/view.py
+++ src/zonnecel/view.py
@@ -1,2 +1,21 @@
+from models.model import ZonnecelExperiment
+import matplotlib.pyplot as plt
 
+port = 'ASRL5::INSTR'
+experiment = ZonnecelExperiment(port)
+
+def run_experiment():
+    U, I = experiment.scan(0, 1023)
+
+    # plotting measurements 
+    plt.fig_d1 = plt.figure('Plot Voltage and Current')
+    # plt.plot(experiment.self.voltages , experiment.self.currents, '.')
+    plt.plot(U , I, '.')
+    # plt.errorbar( U, I, yerr = I_std, fmt = 'o', label = "LED with error")
+    plt.xlabel('Voltage (V)')
+    plt.ylabel('Current (A)')
+    plt.legend(loc='upper right', shadow=True, ncol=1)
+
+    plt.show()
              
+run_experiment()
