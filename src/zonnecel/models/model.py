9d22a5724ff6f3d2d117f9ebb75219edd91045fb
rdvanturnhout <117833887+rdvanturnhout@users.noreply.github.com>
2022-12-06 12:11:06 +0100

    model

--- src/zonnecel/models/model.py
+++ src/zonnecel/models/model.py
@@ -1,1 +1,24 @@
-print("hello")
+from controllers.arduino_device import list_devices, ArduinoVISADevice
+
+print(list_devices())
+
+class DiodeExperiment:
+# Creating lists
+    def __init__(self, port):
+        self.device = ArduinoVISADevice(port)
+
+    def scan(self, start, stop):
+        """Run experiment with ADC inputs in range(start, stop)
+
+        Args:
+            start (int): ADC value where the experiment starts (0 - 1023)
+            stop (int): ADC value where the experiment stops (0 - 1023)
+
+
+        """        
+        R = 220
+                
+        for i in range (start,stop):
+            self.device.set_output_value(i)
+            U1 = 
+            U2 = 
--- src/zonnecel/view.py
+++ src/zonnecel/view.py
@@ -1,3 +1,2 @@
-from controllers.arduino_device import list_devices
 
-print(list_devices())
+             
